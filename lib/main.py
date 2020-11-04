cc68fbd3dde9e2a1bb21cb1b0f4567544c94cd93
ctuguina <ctuguina@uw.edu>
2020-11-03 19:30:33 -0800

    add options in main
    -Options for NLP or Least Squares added
    -Not ready yet added

--- lib/main.py
+++ lib/main.py
@@ -1,8 +1,11 @@
 import os
 import random
+import requests
 
 import discord
+from webbot import Browser
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,14 +18,24 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("https://quizlet.com/"):
+    if message.content == '!options':
+        channel = message.channel
         author = message.author
-        response = "valid"
-        await message.channel.send(response)
-    else:
-        await message.channel.send("invalid")
+        await channel.send('Choose:')
+        await channel.send('Least Squares')
+        await channel.send('or')
+        await channel.send('NLP')
+
+        def check_math(m):
+            return m.content == 'Least Squares' and m.channel == channel and m.author == author
+
+        def check_nlp(m):
+            return m.content == 'NLP' and m.channel == channel and m.author == author
+
+        def check_all(m):
+            return check_math(m) or check_nlp(m)
+
+        msg = await client.wait_for('message', check=check_all)
+        await channel.send('Nice choice {.author}! But this is not ready yet.'.format(msg))
 
 client.run(TOKEN)
